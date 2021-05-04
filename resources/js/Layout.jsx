b0cb03ec787417de8273597bd0c3dfae525ee0e1
Serabass <serabass565@gmail.com>
2021-05-04 21:27:37 +0600

    upd

--- resources/js/Layout.jsx
+++ resources/js/Layout.jsx
@@ -28,13 +28,16 @@ export default function AppLayout({title, children}) {
                 </Menu>
             </Header>
             <Content style={{padding: '0 50px'}}>
-                <Breadcrumb style={{margin: '16px 0'}}>
+                <span>
+                    <a href="https://github.com/Serabass/inertiajs-sandbox" style={{float: 'right'}} target="_blank">Github</a>
+                </span>
+                <Breadcrumb style={{margin: '16px 0'}} hidden>
                     <Breadcrumb.Item>Home</Breadcrumb.Item>
                     <Breadcrumb.Item>List</Breadcrumb.Item>
                     <Breadcrumb.Item>App</Breadcrumb.Item>
                 </Breadcrumb>
                 <Layout className="site-layout-background" style={{padding: '24px 0'}}>
-                    <Sider className="site-layout-background" width={200}>
+                    <Sider className="site-layout-background" width={200} hidden>
                         <Menu
                             mode="inline"
                             defaultSelectedKeys={['1']}
